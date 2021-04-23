7d9cbc4f93481801d25cef76ca7b2daffb3c1f80
Puleeno Nguyen <puleeno@gmail.com>
2021-04-23 20:27:28 +0700

    Create plugin file

---
+++ the-migration-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: The Migration Plugin Name
+ * Plugin URI: https://github.com/puleeno/rake-wordpress-migration-example/
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Use Rake migration framework to migrate other website to WordPress
+ */
