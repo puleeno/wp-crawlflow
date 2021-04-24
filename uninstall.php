67b4c5e37c55a412c5a0e716ac24166af966ca9f
Puleeno Nguyen <puleeno@gmail.com>
2021-04-24 16:39:06 +0700

    Call remove tables method when uninstall the plugin

--- uninstall.php
+++ uninstall.php
@@ -2,6 +2,8 @@
 namespace Migrator;
 
 use App\TaskRunner;
+use Ramphor\Rake\Initialize;
+use Puleeno\Rake\WordPress\Driver;
 
 class Uninstaller
 {
@@ -10,9 +12,16 @@ class Uninstaller
         wp_clear_scheduled_hook(TaskRunner::TASK_CRON_NAME);
     }
 
+    public function remove_rake_db_tables()
+    {
+        $initializer = new Initialize(new Driver());
+        $initializer->removeDbTables();
+    }
+
     public function uninstall()
     {
         $this->remove_cron_events();
+        $this->remove_rake_db_tables();
     }
 }
 
