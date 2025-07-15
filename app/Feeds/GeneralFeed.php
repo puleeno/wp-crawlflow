1f102e3d0c9f60865a2439eb1627319f6583c7f9
Puleeno Nguyen <puleeno@gmail.com>
2025-07-15 09:45:40 +0700

    Update general feed

--- app/Feeds/GeneralFeed.php
+++ app/Feeds/GeneralFeed.php
@@ -2,15 +2,23 @@
 
 namespace CrawlFlow\Feeds;
 
+use CrawlFlow\Interfaces\GeneralFeedInterface;
+use Ramphor\Rake\Facades\Resources;
+use Ramphor\Rake\Link;
+
 if (!defined('ABSPATH')) {
     exit('Cheating huh?');
 }
 
 use Ramphor\Rake\Abstracts\Feed;
 
-class GeneralFeed extends Feed
+class GeneralFeed extends Feed implements GeneralFeedInterface
 {
     const NAME = 'general';
+
+    protected $args = [];
+
+    protected $url;
 
     public function get_name()
     {
@@ -19,10 +27,18 @@ class GeneralFeed extends Feed
 
     public function fetch()
     {
+        // push current URL to database
+        $link = new Link(
+            $this->getUrl(),
+            null,
+            array_get($this->args, 'trimLastSplashURL', true)
+        );
+        $this->insertCrawlUrl($link);
     }
 
     public function valid()
     {
+        return preg_match('/^https?:/', $this->getUrl());
     }
 
     public function rewind()
@@ -31,5 +47,16 @@ class GeneralFeed extends Feed
 
     public function next()
     {
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+
+    public function getUrl()
+    {
+        return $this->url;
     }
 }
---
+++ app/Interfaces/GeneralFeedInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CrawlFlow\Interfaces;
+
+interface GeneralFeedInterface
+{
+    public function setUrl($url);
+
+
+    public function getUrl();
+}
